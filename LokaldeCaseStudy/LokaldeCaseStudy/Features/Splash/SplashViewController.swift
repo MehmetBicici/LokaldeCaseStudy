126f3c99403647c446624668dfc74924b3869a31
Mehmet Biçici <mehmet.bcc.07@gmail.com>
2026-06-02 01:45:38 +0300

    create splash and base viewControllers

--- LokaldeCaseStudy/LokaldeCaseStudy/Features/Splash/SplashViewController.swift
+++ LokaldeCaseStudy/LokaldeCaseStudy/Features/Splash/SplashViewController.swift
@@ -7,23 +7,61 @@
 
 import UIKit
 
-class SplashViewController: UIViewController {
+private enum Constants {
+    static let backgroundColor: UIColor = .splashBackground
+    
+    enum SplashImageView {
+        static let image: UIImage = .splash
+    }
+    
+    enum SplashTitleLabel {
+        static let text: String = String(localized: "splash_title", defaultValue: "OKALDE")
+        static let font: UIFont = .splashTitle
+        static let textColor: UIColor = .white
+    }
+}
 
+final class SplashViewController: BaseViewController {
+
+    @IBOutlet private weak var splashImageView: UIImageView!
+    @IBOutlet private weak var splashTitleLabel: UILabel!
+    
+    private let viewModel = SplashViewModel()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        prepareUI()
+        prepareViewModel()
+        viewModel.startSplashSequence()
     }
+}
 
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+private extension SplashViewController {
+    func prepareUI() {
+        self.view.backgroundColor = Constants.backgroundColor
+        prepareSplashImageView()
+        prepareSplashTitleLabel()
     }
-    */
+    
+    func prepareSplashImageView() {
+        splashImageView.image = Constants.SplashImageView.image
+        splashImageView.makeCircular()
+    }
+    
+    func prepareSplashTitleLabel() {
+        splashTitleLabel.textColor = Constants.SplashTitleLabel.textColor
+        splashTitleLabel.font = Constants.SplashTitleLabel.font
+        splashTitleLabel.text = Constants.SplashTitleLabel.text
+    }
+    
+    func prepareViewModel() {
+        viewModel.delegate = self
+    }
+}
 
+// MARK: - SplashViewModelDelegate
+extension SplashViewController: SplashViewModelDelegate {
+    func navigateProvidersPage() {
+        view.window?.switchRootViewController(UIViewController())
+    }
 }
--- LokaldeCaseStudy/LokaldeCaseStudy/Features/Splash/SplashViewModel.swift
+++ LokaldeCaseStudy/LokaldeCaseStudy/Features/Splash/SplashViewModel.swift
@@ -5,3 +5,23 @@
 //  Created by Mehmet Biçici on 1.06.2026.
 //
 
+import Foundation
+
+private enum Constants {
+    static let delay: CGFloat = 1.0
+}
+
+protocol SplashViewModelDelegate: AnyObject {
+    func navigateProvidersPage()
+}
+
+final class SplashViewModel {
+    weak var delegate: SplashViewModelDelegate?
+    
+    func startSplashSequence() {
+        DispatchQueue.main.asyncAfter(deadline: .now() + Constants.delay) { [weak self] in
+            guard let self = self else { return }
+            delegate?.navigateProvidersPage()
+        }
+    }
+}
